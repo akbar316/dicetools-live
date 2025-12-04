31fdf6c126295d82b1df29428b72728524eb14e2
akbar316 <akbar.com2@gmail.com>
2025-12-04 14:15:19 +0500

    Delete constants.tsx

--- constants.tsx
+++
@@ -1,4 +0,0 @@
-// This file is deprecated. Please use 'data/tools-registry', 'data/categories', 'data/faqs'.
-export * from './data/tools-registry';
-export * from './data/categories';
-export * from './data/faqs';
